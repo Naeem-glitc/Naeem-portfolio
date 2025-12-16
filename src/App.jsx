9611b3c1dabcdc68559073cf8b963a56c0c42371
Naeem Abbas <naeem401080@gmail.com>
2025-12-16 22:12:02 +0500

    Dock UI is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Navbar, Welcome } from '#components'
+import { Navbar, Welcome, Dock } from '#components'
 import React from 'react'
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Welcome/>
+      <Dock/>
     </main>
   )
 }
